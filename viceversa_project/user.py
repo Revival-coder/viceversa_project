73c20e682e6984514f32c7db2039242587c96f9c
unknown <nastia8425@gmail.com>
2021-09-09 02:32:55 +0300

    Four commit

--- viceversa_project/user.py
+++ viceversa_project/user.py
@@ -4,5 +4,7 @@ def home(request):
     return render(request,'home.html')
 def reverse(request):
     text_user=request.GET['comment']
+    words=text_user.split()
+    number=len(words)
     reversed=text_user[::-1]
-    return render(request,'reverse.html',{'comment':text_user,'reversedtext':reversed})
+    return render(request,'reverse.html',{'comment':text_user,'reversedtext':reversed,'number':number,})
